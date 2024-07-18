73045d348641e51126805beef17973871abbb24a
Nabin Singh <nabin@skilluplabs.com.au>
2024-07-18 19:03:51 +1000

    Add display users function

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,7 +14,29 @@ function Expense(description, amount) {
 }
 
 function SplitCostApp() {
-    this.unsettledAmount = 0;
+    this.unsettledAmount = 20.00;
     this.users = [];
-    this.expenses = []
+    this.expenses = [];
+
+    this.displayUnsettledAmount = function() {
+        document.querySelector(".amount").textContent = `$${this.unsettledAmount}`; 
+    }
+
+    this.addUser = function(name, email, mobile, photo) {
+        const user = new User(name, email, mobile, photo);
+        this.users.push(user);
+    }
+
+    this.displayUsers = function() {
+        let userElements = '';
+        for(let user of this.users) {
+            userElements += `<div><img src="${user.photo}" alt="${user.name}" /></div>`
+        }
+        document.querySelector(".users-wrapper").innerHTML = userElements;
+    }
 }
+
+const splitCostApp = new SplitCostApp();
+splitCostApp.displayUnsettledAmount();
+splitCostApp.addUser('Alex', 'alex@gmail.com', '0420400168', 'https://randomuser.me/api/portraits/men/90.jpg');
+splitCostApp.displayUsers();
